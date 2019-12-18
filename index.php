61461c4b7bf7b18fd18b40bdde073687e7d208d8
Cmell <clmellinger@gmail.com>
2019-12-17 23:42:14 -0700

    added group text

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ $file = fopen("conds.csv","r");
 
 $affirmationcond = 'none';
 $ratcond = 'none';
+$grouptext = 'none';
 while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
   if ($data[0] == $mturkcode) {
     $affirmationcond = $data[1];
     $ratcond = $data[2];
+    $grouptext = $data[3];
     break;
   }
 }
@@ -22,6 +24,7 @@ $retval = Array();
 
 $retval['affirmcond'] = $affirmationcond;
 $retval['ratcond'] = $ratcond;
+$retval['grouptext'] = $grouptext;
 
 header('Content-type: application/json');
 exit(json_encode($retval));
